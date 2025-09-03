a9ab508a0e484be8cede64c9c4f5dc74568395bb
Aswathy <aswathyj118@gmail.com>
2025-09-03 22:02:52 +0530

    removed js files

--- Facade/main.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var OrderFacade_1 = require("./facade/OrderFacade");
-// client only calls the OderFacade and not the other services.
-var orderFacade = new OrderFacade_1.OrderFacade();
-orderFacade.placeOrder("user123", "product456", 999);
-orderFacade.placeOrder("user456", "product456", 999);
-orderFacade.placeOrder("user456", "product456", 999);
-orderFacade.placeOrder("user456", "product456", 999);
